39af7a20926a384226fd40a24a7b88e22fbbbb49
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2020-02-07 00:33:26 +0800

    yaml configuration implemented

--- src/main/java/com/zhanglei/JLDFS/App.java
+++ src/main/java/com/zhanglei/JLDFS/App.java
@@ -1,13 +1,36 @@
 package com.zhanglei.JLDFS;
+import java.net.URL;
+//import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    
+
+    private static Map parseYaml(String cf) {
+        Yaml yaml = new Yaml();
+        URL url = App.class.getClassLoader().getResource("config.yaml");
+        //FileInputStream in = (FileInputStream)App.class.getResourceAsStream(cf);
+        Map cfg = null;
+        try {
+            cfg = (Map) yaml.load(new FileInputStream(url.getFile()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return cfg;
+        
+    }
     public static void main( String[] args )
     {
+        String config_path = "config.yaml";
+        System.out.println(parseYaml(config_path));
         System.out.println( "Hello World!" );
     }
 }
